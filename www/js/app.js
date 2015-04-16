f6a0ed4dbae5040ff11dfcd8ada273374a97e24a
Daniel Matthew <hello@danmatthew.co.uk>
2015-04-16 11:53:39 +0100

    Ability to add data

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,38 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  $scope.tasks = [];
+
+  // Create and load modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = '';
+  };
+
+  // Open task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
